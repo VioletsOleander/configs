6992e3c76a9439eb01c24c8a3d009d19767e8a78
VioletsOleander <1377232072@qq.com>
2026-04-30 18:09:12 +0800

    nvim(plugin/completion): let tab also do snippet foward, let c-h show signature
    Also enable automatic signature help

--- nvim/lua/plugins/edit/completion.lua
+++ nvim/lua/plugins/edit/completion.lua
@@ -5,11 +5,11 @@ elseif vim.g.user_use_builtin_completion then
 end
 
 ---@module "lazy"
+---@module "blink.cmp"
 
 ---@type LazyPluginSpec
 local copilot = {
 	"zbirenbaum/copilot.lua",
-	event = "InsertEnter",
 	cmd = "Copilot",
 	opts = {
 		suggestion = { enabled = false },
@@ -18,38 +18,43 @@ local copilot = {
 	},
 }
 
+---@type blink.cmp.Config
+local blink_opts = {
+	keymap = {
+		preset = "default",
+		["<Tab>"] = { "select_and_accept", "snippet_forward", "fallback" },
+		["<C-k>"] = { "select_prev", "fallback" },
+		["<C-j>"] = { "select_next", "fallback" },
+		["<C-p>"] = { "select_prev", "fallback" },
+		["<C-n>"] = { "select_next", "fallback" },
+		["<C-f>"] = { "show", "show_documentation", "fallback" },
+		["<C-h>"] = { "show_signature", "hide_signature", "fallback" },
+	},
+	signature = { enabled = true, window = { show_documentation = true } },
+	cmdline = { enabled = false },
+	appearance = { nerd_font_variant = "mono" },
+	completion = { documentation = { auto_show = true } },
+	sources = {
+		default = { "lsp", "copilot", "path", "snippets", "buffer" },
+		providers = {
+			copilot = {
+				name = "copilot",
+				module = "blink-copilot",
+				score_offset = 100,
+				async = true,
+			},
+		},
+	},
+	fuzzy = { implementation = "prefer_rust_with_warning" },
+}
+
 ---@type LazyPluginSpec
 local blink_cmp = {
 	"saghen/blink.cmp",
 	event = { "InsertEnter" },
 	dependencies = { "rafamadriz/friendly-snippets", "fang2hou/blink-copilot" },
 	version = "1.*",
-	opts = {
-		keymap = {
-			preset = "default",
-			["<Tab>"] = { "select_and_accept", "fallback" },
-			["<C-k>"] = { "select_prev", "fallback" },
-			["<C-j>"] = { "select_next", "fallback" },
-			["<C-p>"] = { "select_prev", "fallback" },
-			["<C-n>"] = { "select_next", "fallback" },
-			["<C-f>"] = { "show", "show_documentation", "fallback" },
-		},
-		cmdline = { enabled = false },
-		appearance = { nerd_font_variant = "mono" },
-		completion = { documentation = { auto_show = false } },
-		sources = {
-			default = { "lsp", "copilot", "path", "snippets", "buffer" },
-			providers = {
-				copilot = {
-					name = "copilot",
-					module = "blink-copilot",
-					score_offset = 100,
-					async = true,
-				},
-			},
-		},
-		fuzzy = { implementation = "prefer_rust_with_warning" },
-	},
+	opts = blink_opts,
 	opts_extend = { "sources.default" },
 }
 
