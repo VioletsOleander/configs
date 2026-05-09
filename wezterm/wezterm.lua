d8c541bec24c9e6b1bdd1650158c0f80c46d8ccb
VioletsOleander <1377232072@qq.com>
2026-05-09 22:23:58 +0800

    wezterm: add font configs
    After comparsion, I have to say windows terminal is just so good
    looking, wezterm and alacritty both can not have the same visual quality
    as windows terminal.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,5 +3,10 @@ local config = {}
 
 config.color_scheme = "AtomOneLight"
 config.default_prog = { "nu" }
+config.font = wezterm.font_with_fallback({
+	"Fira Code Retina",
+	"FiraCode Nerd Font",
+})
+config.font_size = 11.5
 
 return config
