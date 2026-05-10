6113ad37d605149a9ae8d6d3e21beba62ca0df47
VioletsOleander <1377232072@qq.com>
2026-05-10 11:08:22 +0800

    nvim(autocmd): delete insert mode lsp keymaps
    In order to let typing more smooth, since the beam vertical line update
    delay is annoying

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -70,17 +70,13 @@ au("LspAttach", {
 		-- LSP shows
 		map_local("n", "gr", vim.lsp.buf.references, "Show references")
 
-		map_local({ "n", "i" }, "gk", function()
-			vim.lsp.buf.signature_help({ close_events = { "CursorMoved", "BufHidden", "InsertLeave" } })
-		end, "Show signature help")
-		map_local({ "n", "i" }, "gK", function()
+		map_local("n", "gk", vim.lsp.buf.signature_help, "Show signature help")
+		map_local("n", "gK", function()
 			vim.lsp.buf.signature_help({ close_events = { "BufHidden" } })
 		end, "Show persist signature help (requires manual close)")
 
-		map_local({ "n", "i" }, "ga", function()
-			vim.diagnostic.open_float({ close_events = { "CursorMoved", "BufHidden", "InsertLeave" } })
-		end, "Show diagnostics")
-		map_local({ "n", "i" }, "gA", function()
+		map_local("n", "ga", vim.diagnostic.open_float, "Show diagnostics")
+		map_local("n", "gA", function()
 			vim.diagnostic.open_float({ close_events = { "BufHidden" } })
 		end, "Show persist diagnostic (requires manual close)")
 
