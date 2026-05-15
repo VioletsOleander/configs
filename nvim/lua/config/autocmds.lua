f621a3aaa52f2981008b17cbbfc09280053c0347
VioletsOleander <1377232072@qq.com>
2026-05-15 10:39:17 +0800

    nvim(autocmd): disable spell for lsp float window

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -85,6 +85,17 @@ au("LspAttach", {
 		map_local("n", "<Leader>r", vim.lsp.buf.rename, "Rename symbol")
 
 		map_local("n", "<Leader>a", vim.lsp.buf.code_action, "Code action")
+
+		-- Disable spell check for lsp float window
+		local _open_floating_preview = vim.lsp.util.open_floating_preview
+		---@diagnostic disable-next-line: duplicate-set-field
+		vim.lsp.util.open_floating_preview = function(contents, syntax, opts)
+			local bufnr, winnr = _open_floating_preview(contents, syntax, opts)
+			if vim.api.nvim_win_is_valid(winnr) then
+				vim.api.nvim_set_option_value("spell", false, { scope = "local", win = winnr })
+			end
+			return bufnr, winnr
+		end
 	end,
 })
 
