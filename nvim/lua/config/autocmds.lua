a9c45bfcf36baa8c9ae7f96f9c130555088782de
VioletsOleander <1377232072@qq.com>
2026-05-18 10:18:27 +0800

    nvim(autocmd): disable color column for lsp float window

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -58,6 +58,7 @@ local treesitter_fts = {
 	"nu",
 	-- Other
 	"gitcommit",
+	"gitignore",
 	"help",
 }
 
@@ -121,13 +122,14 @@ au("LspAttach", {
 
 		map_local("n", "<Leader>a", vim.lsp.buf.code_action, "Code action")
 
-		-- Disable spell check for lsp float window
+		-- Disable spell check, colorcolumn for lsp float window
 		local _open_floating_preview = vim.lsp.util.open_floating_preview
 		---@diagnostic disable-next-line: duplicate-set-field
 		vim.lsp.util.open_floating_preview = function(contents, syntax, opts)
 			local bufnr, winnr = _open_floating_preview(contents, syntax, opts)
 			if vim.api.nvim_win_is_valid(winnr) then
 				vim.api.nvim_set_option_value("spell", false, { scope = "local", win = winnr })
+				vim.api.nvim_set_option_value("colorcolumn", "", { scope = "local", win = winnr })
 			end
 			return bufnr, winnr
 		end
