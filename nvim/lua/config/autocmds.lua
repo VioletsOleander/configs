7edd595123da143689cc416f6231c4744bee5eb0
VioletsOleander <1377232072@qq.com>
2026-05-19 10:40:13 +0800

    nvim(lsp): move open_floting_preview replacement out of autocmd, let <C-s> not disapper on CursorMoveI
    Since leaving open_floting_preview in autocmd will cause this
    replacement be stacked each time lsp is attached

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -116,23 +116,14 @@ au("LspAttach", {
 		map_local("n", "gA", function()
 			vim.diagnostic.open_float({ close_events = { "BufHidden" } })
 		end, "Show persist diagnostic (requires manual close)")
+		map_local("i", "<C-s>", function()
+			vim.lsp.buf.signature_help({ close_events = { "InsertLeave", "BufHidden" } })
+		end, "Show persist signature help")
 
 		-- LSP actions
 		map_local("n", "<Leader>r", vim.lsp.buf.rename, "Rename symbol")
 
 		map_local("n", "<Leader>a", vim.lsp.buf.code_action, "Code action")
-
-		-- Disable spell check, colorcolumn for lsp float window
-		local _open_floating_preview = vim.lsp.util.open_floating_preview
-		---@diagnostic disable-next-line: duplicate-set-field
-		vim.lsp.util.open_floating_preview = function(contents, syntax, opts)
-			local bufnr, winnr = _open_floating_preview(contents, syntax, opts)
-			if vim.api.nvim_win_is_valid(winnr) then
-				vim.api.nvim_set_option_value("spell", false, { scope = "local", win = winnr })
-				vim.api.nvim_set_option_value("colorcolumn", "", { scope = "local", win = winnr })
-			end
-			return bufnr, winnr
-		end
 	end,
 })
 
