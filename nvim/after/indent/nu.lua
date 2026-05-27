93fc1f1d8a131909bef4dbde6243be8afe2acad9
VioletsOleander <1377232072@qq.com>
2026-05-27 21:02:00 +0800

    nvim: configure nu indent

---
+++ nvim/after/indent/nu.lua
@@ -0,0 +1,5 @@
+-- The indent/nu.vim file will override tabstop
+-- and shiftwidth set in ftplugin, therefore we
+-- need to override it here
+vim.opt_local.tabstop = 4
+vim.opt_local.shiftwidth = 4
