18c6619636d2b541fa2e8c897e2912c329219edd
VioletsOleander <1377232072@qq.com>
2026-05-28 15:56:58 +0800

    nvim(ftplugin): for markdown, if float window, do nothing

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -1,5 +1,11 @@
+-- Directly return if current buffer is for floating window
+if vim.bo.buftype == "nofile" then
+	return
+end
+
 vim.opt_local.spell = true
 
 vim.opt_local.autocomplete = false
 vim.opt_local.completeopt = "menu,popup"
+
 vim.b.completion = false
