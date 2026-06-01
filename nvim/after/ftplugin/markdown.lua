3815cacf5661d65c03feeb826c1425fa715ddf1d
VioletsOleander <1377232072@qq.com>
2026-06-01 23:34:44 +0800

    nvim(ftplugin): update markdown

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -3,8 +3,11 @@ if vim.bo.buftype == "nofile" then
   return
 end
 
-vim.opt_local.spell = true
+vim.treesitter.start()
 
+vim.opt_local.textwidth = 120
+vim.opt_local.colorcolumn = "+1"
+vim.opt_local.spell = true
 vim.opt_local.autocomplete = false
 vim.opt_local.completeopt = "menu,popup"
 
