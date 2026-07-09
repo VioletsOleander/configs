94f161fe64cd16907415a70b5efa23f2b2333c72
VioletsOleander <1377232072@qq.com>
2026-07-09 18:09:55 +0800

    nvim: add clipboard support for wsl

--- nvim/lua/config/misc.lua
+++ nvim/lua/config/misc.lua
@@ -10,3 +10,20 @@ vim.api.nvim_create_user_command("H", "vertical help <args>", { nargs = "*", com
 -- UI2 (flicker in wildtrigger, disable now)
 -- require("vim._core.ui2").enable({ enable = true })
 -- vim.opt.cmdheight = 0
+
+-- WSL
+-- Refers to: https://vi.stackexchange.com/questions/42305/neovim-following-the-instructions-in-h-clipboard-wsl-not-work-it-shows-no
+if vim.fn.has("wsl") then
+  vim.g.clipboard = {
+    name = "win32yank-wsl",
+    copy = {
+      ["+"] = "win32yank.exe -i --crlf",
+      ["*"] = "win32yank.exe -i --crlf"
+    },
+    paste = {
+      ["+"] = "win32yank.exe -o --lf",
+      ["*"] = "win32yank.exe -o --lf"
+    },
+    cache_enabled = true,
+  }
+end
