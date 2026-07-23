963f7a622a4c1233e9cf609ad5515783dfee9077
VioletsOleander <1377232072@qq.com>
2026-07-23 20:30:10 +0800

    nvim: vim.g.edge_disable_italic_comment = 1

--- nvim/lua/plugins/ui/color.lua
+++ nvim/lua/plugins/ui/color.lua
@@ -33,6 +33,7 @@ local edge = {
   priority = 1000,
   config = function()
     vim.g.edge_better_performance = 1
+    vim.g.edge_disable_italic_comment = 1
     vim.cmd("colorscheme edge")
   end,
 }
