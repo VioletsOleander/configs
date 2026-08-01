34576b5621c81f49ab6411d3f2ce54e6918fc2ef
VioletsOleander <1377232072@qq.com>
2026-08-01 15:00:26 +0800

    nvim(ftplugin): format options remove tcro for gitcommit, markdown

--- nvim/after/ftplugin/gitcommit.lua
+++ nvim/after/ftplugin/gitcommit.lua
@@ -1,6 +1,9 @@
 vim.treesitter.start()
 
 vim.opt_local.textwidth = 100
+-- Do not autowrap, auto insert comment leader
+vim.opt_local.formatoptions:remove({ "t", "c", "r", "o" })
+
 vim.opt_local.spell = true
 vim.opt_local.completeopt = "menu,popup"
 
