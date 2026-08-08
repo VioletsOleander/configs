70fd79e0f17172151ab00a3609f63b1ee8060a2e
VioletsOleander <1377232072@qq.com>
2026-08-08 16:26:58 +0800

    nvim(usercmd): add relative jump up/down

--- nvim/lua/config/usercmds.lua
+++ nvim/lua/config/usercmds.lua
@@ -46,3 +46,48 @@ end, { desc = "Enable or disable showing attention attracting colorcolumn" })
 com("ToggleLspInlayHint", function()
   vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
 end, { desc = "Enable or disable showing attention attracting lsp inlay hint." })
+
+-- Relative jump up/down
+local function relative_jump_up()
+  vim.wo.relativenumber = true
+
+  vim.ui.input({ prompt = "Enter number of lines to jump up: ", scope = "cursor" }, function(input)
+    local number = tonumber(input)
+
+    if number then
+      local cursor = vim.api.nvim_win_get_cursor(0)
+      vim.api.nvim_win_set_cursor(0, { math.max(cursor[1] - number, 1), cursor[2] })
+    else
+      vim.notify("Invalid input, expect numbers")
+    end
+
+    vim.wo.relativenumber = false
+  end)
+end
+
+local function relative_jump_down()
+  vim.wo.relativenumber = true
+
+  vim.ui.input(
+    { prompt = "Enter number of lines to jump down: ", scope = "cursor" },
+    function(input)
+      local number = tonumber(input)
+
+      if number then
+        local cursor = vim.api.nvim_win_get_cursor(0)
+        local line_count = vim.api.nvim_buf_line_count(0)
+        vim.api.nvim_win_set_cursor(0, { math.min(cursor[1] + number, line_count), cursor[2] })
+      else
+        vim.notify("Invalid input, expect numbers")
+      end
+
+      vim.wo.relativenumber = false
+    end
+  )
+end
+
+com("RelativeJumpUp", relative_jump_up, { desc = "Prompt user for a relative jump up." })
+vim.keymap.set("n", "gk", relative_jump_up, { desc = "Prompt user for a relative jump up." })
+
+com("RelativeJumpDown", relative_jump_down, { desc = "Prompt user for a relative jump down." })
+vim.keymap.set("n", "gj", relative_jump_down, { desc = "Prompt user for a relative jump down." })
